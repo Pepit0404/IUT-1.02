a121583132a78d11cece3e92c910af60de15aa8f
pinto <samuel.pinto@etu.uca.fr>
2024-01-12 08:14:46 +0100

    liberé!

--- Offre/test.c
+++ Offre/test.c
@@ -9,15 +9,23 @@
 
 int main(void){
     int size=0, max=5;
+    
     Offre **o;
     o=(Offre **)malloc(5*sizeof(Offre *));
     if(o==NULL){printf("PB de malloc pour offre !\n");exit(1);}
     size=readOffre(o, size, &max, "devis.txt");
-    //afficherTout(o, size);
-    printf("\n\n");
+    /*
+    afficherTout(o, size);
     sortByCost(o, size);
     afficherTout(o, size);
-    t=chargerTache(o, size);
-    afficherTaches(t, size);
+    *//*
+    Tache **t;
+    t=(Tache **)malloc(5*sizeof(Tache *));
+    if(t==NULL){printf("PB de malloc pour tache !\n");exit(1);}*/
+    Tache *t[100];
+    int s;
+    s=fChargementTache(t, 100, o, size);
+    afficherTaches(t, s);
+    printf("\n");
     return 0;
 }
